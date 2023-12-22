283cb2280936075d78ff8a23a1db52115406ec8f
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-12-22 10:59:25 -0300

    Criando interface de perfil

--- app/src/main/java/com/myo/whatsapp/PerfilActivity.kt
+++ app/src/main/java/com/myo/whatsapp/PerfilActivity.kt
@@ -2,10 +2,31 @@ package com.myo.whatsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myo.whatsapp.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityPerfilBinding.inflate( layoutInflater )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_perfil)
+        setContentView( binding.root )
+
+        inicializarToolbar()
+
+    }
+
+    private fun inicializarToolbar() {
+
+        val toolbar = binding.includeMTBPerfil.mtbPrincipal
+
+        setSupportActionBar( toolbar )
+
+        supportActionBar?.apply {
+            title = "Editar Perfil"
+            setDisplayHomeAsUpEnabled( true )
+        }
     }
 }
