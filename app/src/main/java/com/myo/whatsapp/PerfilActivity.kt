adfdfe321623730f31a2cc96e751dead359de317
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2023-12-22 14:16:37 -0300

    Solicitando permissões

--- app/src/main/java/com/myo/whatsapp/PerfilActivity.kt
+++ app/src/main/java/com/myo/whatsapp/PerfilActivity.kt
@@ -1,7 +1,11 @@
 package com.myo.whatsapp
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.myo.whatsapp.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
@@ -10,12 +14,55 @@ class PerfilActivity : AppCompatActivity() {
         ActivityPerfilBinding.inflate( layoutInflater )
     }
 
+    private var temPermissaoCamera = false
+    private var temPermissaoGaleria = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView( binding.root )
 
         inicializarToolbar()
+        solicitarPermissoes()
 
+    }
+
+    private fun solicitarPermissoes() {
+
+        // Verificar se usuário possui permissão
+        temPermissaoCamera = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+
+        temPermissaoGaleria = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_MEDIA_IMAGES
+        ) == PackageManager.PERMISSION_GRANTED
+
+        // Lista de permissões negadas
+        val listaPermissoesNegadas = mutableListOf<String>()
+
+        if ( !temPermissaoCamera ) {
+            listaPermissoesNegadas.add( Manifest.permission.CAMERA )
+        }
+
+        if ( !temPermissaoGaleria ) {
+            listaPermissoesNegadas.add( Manifest.permission.READ_MEDIA_IMAGES )
+        }
+
+        if ( listaPermissoesNegadas.isNotEmpty() ) {
+
+            // Solicitar múltiplas permissões
+            val gerenciadorPermissoes = registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) {permissoes ->
+                temPermissaoCamera = permissoes[ Manifest.permission.CAMERA ]
+                    ?: temPermissaoCamera
+                temPermissaoGaleria = permissoes[ Manifest.permission.READ_MEDIA_IMAGES ]
+                    ?: temPermissaoGaleria
+            }
+            gerenciadorPermissoes.launch( listaPermissoesNegadas.toTypedArray() )
+        }
     }
 
     private fun inicializarToolbar() {
